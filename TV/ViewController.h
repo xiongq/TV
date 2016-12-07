9b3c7dfb5108b1d1cac801cfa10b694997199779
xiong <flyxiongq@gmail.com>
2016-12-07 15:36:43 +0800

    Initial Commit

---
+++ TV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TV
+//
+//  Created by xiong on 2016/12/7.
+//  Copyright © 2016年 xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TV/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TV
+//
+//  Created by xiong on 2016/12/7.
+//  Copyright © 2016年 xiong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
